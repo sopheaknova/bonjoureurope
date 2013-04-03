d655801ebbb5b9fda6ec48f62fd77ae81ff4c5f9
sohpeaknova <novadesign.dev@gmail.com>
2013-04-03 10:09:01 +0700

    Add link to new tab

--- footer.php
+++ footer.php
@@ -12,8 +12,8 @@
 	    	 </div>
 	    	 <div class="sponsors">
 	    	 	   <?php _e('Sponsors by &nbsp;:&nbsp;', 'sptheme'); ?>
-	    	 	   <a href="http://www.airfrance.com/KH/en/local/resainfovol/meilleuresoffres/promo_best_offers.htm" title="Air France"><img src="<?php bloginfo('template_url');?>/images/arfrance.png" width="109" height="12"/></a>
-	    	 	   <a href="http://www.klm.com/" title="KLM"><img src="<?php bloginfo('template_url');?>/images/LOGO-KLM-H.png" width="78" height="30" /></a>
+	    	 	   <a href="http://www.airfrance.com/KH/en/local/resainfovol/meilleuresoffres/promo_best_offers.htm" target="_blank" title="Air France"><img src="<?php bloginfo('template_url');?>/images/arfrance.png" width="109" height="12"/></a>
+	    	 	   <a href="http://www.klm.com/" target="_blank" title="KLM"><img src="<?php bloginfo('template_url');?>/images/LOGO-KLM-H.png" width="78" height="30" /></a>
 	    	 </div>
 	    </div>
 </section>
